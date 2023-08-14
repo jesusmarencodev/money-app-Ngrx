17cad66a2c4adc2eab4a90cdb5b756921f97e99f
Jesus Marenco <jesusmarencodev@gmail.com>
2023-08-14 16:53:55 -0500

    save in db firebase

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,30 +1,41 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth} from '@angular/fire/auth';
-
+import { AngularFireAuth } from '@angular/fire/auth';
+import { map } from 'rxjs/operators';
+import { User } from '../model/user.model';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(private readonly auth:AngularFireAuth) { }
-
-  initAuthListener(){
-    this.auth.authState.subscribe(fuser=> {
+  constructor(
+    private readonly auth: AngularFireAuth,
+    private fireStore: AngularFirestore
+  ) {}
+  initAuthListener() {
+    this.auth.authState.subscribe((fuser) => {
       console.log(fuser);
-    })
+    });
   }
 
-
-  createUser(name:string, email:string, password:string){
-    return this.auth.createUserWithEmailAndPassword(email, password);
+  createUser(name: string, email: string, password: string) {
+    return this.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(({ user }) => {
+        const newUser = new User(user.uid, name, email);
+        return this.fireStore.doc(`${user.uid}/user`).set({...newUser});
+      });
   }
 
-  loginUser(email:string, password:string){
+  loginUser(email: string, password: string) {
     return this.auth.signInWithEmailAndPassword(email, password);
   }
 
-  logout(){
-   return this.auth.signOut()
+  logout() {
+    return this.auth.signOut();
+  }
+
+  isAuth() {
+    return this.auth.authState.pipe(map((fuser) => fuser != null));
   }
 }
